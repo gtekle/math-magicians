e3fbd8625db3b559292f7ec2aec107ea865604ca
tYohannes <teklegy@gmail.com>
2022-02-15 12:17:02 +0300

    Create Header.js and NavBar.js pages

---
+++ src/pages/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import NavBar from './NavBar';
+
+const Header = () => (
+  <>
+    <h1 className="title-text">
+      Math Magicians
+    </h1>
+    <NavBar />
+  </>
+);
+
+export default Header;
---
+++ src/pages/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => (
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>Calculator</li>
+      <li>Quote</li>
+    </ul>
+  </nav>
+);
+
+export default NavBar;
