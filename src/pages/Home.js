532e39d1e7eb3af7c2780cb44349b6598c0c1c1a
tYohannes <teklegy@gmail.com>
2022-02-15 11:46:42 +0300

    Create Home page under pages folder

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <h2 className="welcome-text">
+      Welcome to our page!
+    </h2>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Animi cumque sit quaerat cum, ratione soluta! Eveniet, magnam
+      eum ea excepturi et temporibus laborum at officia!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+      Animi cumque sit quaerat cum, ratione soluta! Eveniet, magnam
+      eum ea excepturi et temporibus laborum at officia!
+    </p>
+  </>
+);
+
+export default Home;
