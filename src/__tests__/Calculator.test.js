cad4dc5c3cd206bc385c51bb65c81723bc298993
tYohannes <teklegy@gmail.com>
2022-02-16 14:46:22 +0300

    Fix eslint errors

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,8 +1,8 @@
 import renderer from 'react-test-renderer';
-import {render, fireEvent, waitFor, screen} from '@testing-library/react'
-import '@testing-library/jest-dom'
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
-import Calculator from "../components/Calculator";
+import Calculator from '../components/Calculator';
 
 it('Calculator component renders correctly', () => {
   const tree = renderer
@@ -11,11 +11,10 @@ it('Calculator component renders correctly', () => {
   expect(tree).toMatchSnapshot();
 });
 
-
 test('Calculator button click displays button\'s value in the display', async () => {
-  render(<Calculator />)
+  render(<Calculator />);
 
-  fireEvent.click(screen.getByText('9'))
+  fireEvent.click(screen.getByText('9'));
 
-  expect(screen.getByRole('display')).toHaveTextContent('9')
-})
+  expect(screen.getByTestId('display')).toHaveTextContent('9');
+});
